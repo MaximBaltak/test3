32d62451eac30e98506ed791d286c7c1b5fc8899
MaximBaltak <maksimbaltak@yandex.ru>
2021-11-02 21:22:34 +0200

    add parse of colors

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log('connect')
-gi
+let obj={
+    name: 'Max',
+    age: 46,
+    online: true,
+    colors: ['red','blue','black']
+}
+
+console.log(obj.colors.filter(color=>color==='blue').join()[0])
